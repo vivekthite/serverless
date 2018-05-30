b358135ea005470ddc535a65edc66527c2eeca18
VT <vivekanand.thite@synerzip.com>
2018-05-30 13:37:00 +0530

    Completed frontend to call backend APIs. Added predeplou,deploy and postdeploy commands in package.json

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -31,7 +31,7 @@ export default class Login extends Component {
         try {
           await Auth.signIn(this.state.email, this.state.password);
           this.props.userHasAuthenticated(true);
-          this.props.history.push("/");
+         // this.props.history.push("/");
         } catch (e) {
           alert(e.message);
           this.setState({ isLoading: false });
--- notes-app-client/src/containers/NewNote.js
+++ notes-app-client/src/containers/NewNote.js
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./NewNote.css";
+import { API } from "aws-amplify";
+import { s3Upload } from "../libs/awsLib";
 
 export default class NewNote extends Component {
   constructor(props) {
@@ -32,13 +34,34 @@ export default class NewNote extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+  
     if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
       alert("Please pick a file smaller than 5MB");
       return;
     }
-
+  
     this.setState({ isLoading: true });
+  
+    try {
+      const attachment = this.file
+        ? await s3Upload(this.file)
+        : null;
+  
+      await this.createNote({
+        attachment,
+        content: this.state.content
+      });
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
+  
+  createNote(note) {
+    return API.post("notes", "/notes", {
+      body: note
+    });
   }
 
   render() {
